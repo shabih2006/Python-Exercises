23159c7b27709888646f51269eca091f8b517998
shabih2006 <shabih2471@gmail.com>
2026-04-10 12:41:18 +0500

    Add Q1 PFAI Assignment 1

---
+++ Q1 PFAI Assignment 1.py
@@ -0,0 +1,7 @@
+#Q1
+print("Q1")
+text = "This is Q1 of the the PFAI Assignment."
+reversed_text = text[::-1]
+print("Reversed text is:", reversed_text)
+second_characters = text[1::2]
+print("Second characters are:", second_characters)
